5095793dded9f248e7f7adabb6e8ffb0b50507c0
Daniel <daniel.frimu@hoculus.com>
2024-06-11 11:52:31 +0200

    added container and styles to Header component

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,13 +1,17 @@
 export default function Header() {
-  return <header>
-    <h4>MyBlog</h4>
+    return (
+        <header className="bg-red-100">
+            <div className="w-full max-w-[1200px] mx-auto p-5 flex justify-between items-center">
+              <h4>MyBlog</h4>
 
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-      </ul>
-    </nav>
-  </header>
+              <nav>
+                  <ul className="flex gap-5">
+                      <li>Home</li>
+                      <li>About</li>
+                      <li>Services</li>
+                  </ul>
+              </nav>
+            </div>
+        </header>
+    );
 }
